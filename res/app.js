7bd1115bd68da3e2e926ed0ca96efaeb62975eec
啤酒加冰 <mail@sob.im>
2023-12-04 16:41:53 +0800

    Update app.js

--- res/app.js
+++ res/app.js
@@ -9,7 +9,7 @@ new Vue({
   el: "#app",
   data: {
     // Enter your Bing Maps API key here
-    bingMapAPIKEY: process.env.bingMapAPIKEY || "",
+    bingMapAPIKEY: process&&process.env.bingMapAPIKEY ? process.env.bingMapAPIKEY : "ArCk-TwYlg13-bpfLM6dGFw0oeqhRxw7XP428BSsdgRS-m0IyeG6FcQXl_nHQqDc",
     currentLanguage: "en",
     currentTexts: {},
 
